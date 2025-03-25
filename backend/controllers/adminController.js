f95329fe22893686b1dd5b7d42342a810d38372a
Vankhoi3009 <yeuem4730@gmail.com>
2025-03-25 09:05:02 +0700

    111

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,4 +1,4 @@
-import User from "../models/User";
+import User from "../models/User.js";
 
 exports.getAllUsers = async (req, res) => {
     try {
--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,4 @@
-import User from "../models/User";
+import User from "../models/User.js";
 import connectDB from "../utils/connectDB";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
