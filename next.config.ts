50267eb91d5384c0d7c8ec14667f953ae2fc6bf3
Vankhoi3009 <yeuem4730@gmail.com>
2025-03-25 11:02:22 +0700

    123123

--- next.config.ts
+++ next.config.ts
@@ -5,19 +5,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'none'; img-src 'self' data:; script-src 'self'; style-src 'self'; font-src 'self'; connect-src 'self';",
-          },
-        ],
-      },
-    ];
-  },
-};
