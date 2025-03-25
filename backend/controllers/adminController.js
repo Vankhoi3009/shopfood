c81159d058e845c107257c35b1bf943901e1771a
Vankhoi3009 <yeuem4730@gmail.com>
2025-03-25 09:25:15 +0700

    12

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,11 +1,11 @@
 import User from "../models/User.js";
 
-exports.getAllUsers = async (req, res) => {
-    try {
-      const users = await User.find().select("-password");
-      res.json(users);
-    } catch (err) {
-        console.error(err);
-      res.status(500).json({ message: "Server error" });
-    }
-  };
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find().select("-password");
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
